782cfa2af958727337f690e79acaa3e35aba7db2
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-02-20 23:12:57 +0530

    added medium and large subdomain wordlists

--- multi-reconV2(main).py
+++ multi-reconV2(main).py
@@ -142,11 +142,36 @@ def subdomain_finder():
     count = 0
     domain = input("Enter a domain name (example: google.com): ")
     try:
-        with open("subdomains_small.txt", "r") as f:
-            sub_domains = f.read().splitlines()
-    except FileNotFoundError:
-        print("subdomains.txt file not found.")
-        return
+        print("1.Small Scan(3-5s)\n" \
+        "2. Medium scan(15-30s)\n" \
+        "3. Large Scan(1m+)")
+        chi=int(input("Enter choice: "))
+        match chi:
+            case 1:
+                try:
+                    with open("subdomains_small.txt", "r") as f:
+                        sub_domains = f.read().splitlines()
+                except FileNotFoundError:
+                    print("subdomains_small.txt file not found.")
+                    return
+            case 2:
+                try:
+                    with open("subdomains_medium.txt", "r") as f:
+                        sub_domains = f.read().splitlines()
+                except FileNotFoundError:
+                    print("subdomains_medium.txt file not found.")
+                    return
+            case 3:
+                try:
+                    with open("subdomains_large.txt", "r") as f:
+                        sub_domains = f.read().splitlines()
+                except FileNotFoundError:
+                    print("subdomains_large.txt file not found.")
+                    return 
+            case _:
+                print("Enter a valid value")    
+    except:
+        print("Error")
     print("\nScanning for subdomains...\n")
     for sub in sub_domains:
         fulldom = sub + "." + domain
